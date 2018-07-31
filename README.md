7fa349fcec1279ef6ad23799fb5b74e7fec372ba
ukkalamb <bhargava.ukkalam@ipc.com>
2018-07-30 20:36:14 -0400

    reverted all changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Amazon-clone-shopping-website
-Ecommerce shopping website like Amazon
--- config/secret.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    database:'mongodb://root:abc123@ds141641.mlab.com:41641/amazonclone',
-    port:3000,
-    secretKey:"Bh@rg@v@"
-}
